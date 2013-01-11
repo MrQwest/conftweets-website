ca18260e6869c93c230748c39abafd6ab9abe35e
Anthony KIlleen <mrqwest@gmail.com>
2013-01-11 20:32:59 +0000

    changed the filetype to php for some di-nam-ik shizzle. Also added jQ and a jQ sorting plugin

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+
+<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
+<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!-->
+<html class="no-js" lang="en"><!--<![endif]-->
+
+<head>
+	<meta charset="UTF-8">
+	<title>Conftweets.com ~ Tweets, from conferences!</title>
+
+	<!-- If JS is available, changes <html> class to js -->
+	<script>
+		var docElement = document.documentElement;
+		docElement.className = docElement.className.replace(/\bno-js\b/,'') + ' js';
+	</script>
+
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1">
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  	<script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
+
+	<script type="text/javascript" src="js/jq.tinysort.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+</head>
+<body>
+	<h1>conftweets.com</h1>
+	<p>Keep up to date on the twitter-chatter of the following web conferences. Hashtag your tweets with the appropriate hashtags to join in the conversation. See the build-up before each conference and follow along during the day!</p>
+
+	<ul id="conflist">
+		<li data-time="2013-01-23">New Adventures in Web Design</li>
+	</ul>
+
+	<footer>
+		<p>2011-<?php echo date(Y);?> MrQwest</p>
+	</footer>
+</body>
+</html>
