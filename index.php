3a0393c92828d17079d579bfb190ab2d20361ee3
Anthony KIlleen <mrqwest@gmail.com>
2013-01-14 21:41:26 +0000

    some further design tweaks

--- index.php
+++ index.php
@@ -36,40 +36,43 @@
 </head>
 <body>
 	<h1><img src="img/conftweets.png" alt="Conftweets.com" /></h1>
-	<p>Keep up to date on the twitter-chatter of the following web conferences. Hashtag your tweets with the appropriate hashtags to join in the conversation. See the build-up before each conference and follow along during the day!</p>
+	<div class="maincontent">
+		<p>Keep up to date on the twitter-chatter of the following web conferences. Hashtag your tweets with the appropriate hashtags to join in the conversation. See the build-up before each conference and follow along during the day!</p>
 
-	<ul id="conflist">
+		<ul id="conflist">
 
-		<li data-etime="2013-04-19">
-			<h2><a href="/jquk">jQuery UK</a></h2>
-			<p>
-				<span class="location"><i class="ss-icon">location</i>Oxford</span>
-				<span class="date"><i class="ss-icon">date</i>19th April, 2013</span>
-				<span class="webaddress"><a href="http://events.jquery.org/2013/uk/" title="Conference website"><i class="ss-icon">link</i>events.jquery.org/2013/uk/</a></span>
-				<span class="twitteracct"><a href="http://twitter.com/jquk" title="conference twitter acct"><i class="ss-icon ss-social">twitter</i>@jquk</a></span>
-			</p>
-		</li>
+			<li data-etime="2013-04-19">
+				<h2><a href="/jquk">jQuery UK</a></h2>
+				<p>
+					<span class="location"><i class="ss-icon">location</i>Oxford</span>
+					<span class="date"><i class="ss-icon">date</i>19th April, 2013</span>
+					<span class="webaddress"><i class="ss-icon">link</i><a href="http://events.jquery.org/2013/uk/" title="Conference website">events.jquery.org/2013/uk/</a></span>
+					<span class="twitteracct"><i class="ss-icon ss-social">twitter</i><a href="http://twitter.com/jquk" title="conference twitter acct">@jquk</a></span>
+				</p>
+			</li>
 
-		<li data-etime="2013-05-04">
-			<h2><a href="/points">Points</a></h2>
-			<p>
-				<span class="location"><i class="ss-icon">location</i>Brighton</span>
-				<span class="date"><i class="ss-icon">date</i>4th May, 2013</span>
-				<span class="webaddress"><i class="ss-icon">link</i><a href="http://pointsbrighton.co.uk" title="Conference website">pointsbrighton.co.uk</a></span>
-				<span class="twitteracct"><i class="ss-icon ss-social">twitter</i><a href="http://twitter.com/pointsbrighton" title="conference twitter acct">@pointsbrighton</a></span>
-			</p>
-		</li>
+			<a href="/points"><li data-etime="2013-05-04">
+				<h2><a href="/points">Points</a></h2>
+				<p>
+					<span class="location"><i class="ss-icon">location</i>Brighton</span>
+					<span class="date"><i class="ss-icon">date</i>4th May, 2013</span>
+					<span class="webaddress"><i class="ss-icon">link</i><a href="http://pointsbrighton.co.uk" title="Conference website">pointsbrighton.co.uk</a></span>
+					<span class="twitteracct"><i class="ss-icon ss-social">twitter</i><a href="http://twitter.com/pointsbrighton" title="conference twitter acct">@pointsbrighton</a></span>
+				</p>
+			</li>
+		</a>
 
-		<li data-etime="2013-01-23">
-			<h2><a href="naconf">New Adventures 2013</a></h2>
-			<p>
-				<span class="location"><i class="ss-icon">location</i>Nottingham</span>
-				<span class="date"><i class="ss-icon">date</i>23rd January, 2013</span>
-				<span class="webaddress"><i class="ss-icon">link</i><a href="http://newadventuresconf.com" title="Conference website">newadventuresconf.com</a></span>
-				<span class="twitteracct"><i class="ss-icon ss-social">twitter</i><a href="http://twitter.com/naconf" title="conference twitter acct">@naconf</a></span>
-			</p>
-		</li>
-	</ul>
+			<li data-etime="2013-01-23">
+				<h2><a href="naconf">New Adventures 2013</a></h2>
+				<p>
+					<span class="location"><i class="ss-icon">location</i>Nottingham</span>
+					<span class="date"><i class="ss-icon">date</i>23rd January, 2013</span>
+					<span class="webaddress"><i class="ss-icon">link</i><a href="http://newadventuresconf.com" title="Conference website">newadventuresconf.com</a></span>
+					<span class="twitteracct"><i class="ss-icon ss-social">twitter</i><a href="http://twitter.com/naconf" title="conference twitter acct">@naconf</a></span>
+				</p>
+			</li>
+		</ul>
+	</div>
 
 	<footer>
 		<p>2011-<?php echo date(Y);?> MrQwest</p>
